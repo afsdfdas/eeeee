bde1776359829866901ace5e5e1eab0c28017ed4
FightingVG <1607878717@qq.com>
2016-05-26 10:04:06 +0800

    Initial Commit

---
+++ 26/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  26
+//
+//  Created by mac on 16/5/26.
+//  Copyright © 2016年 Fighting-VG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
